a35b2fea3a8083847623bb019e76cfd93ca1f99e
duda maia <125671151+ddbtw@users.noreply.github.com>
2023-07-25 09:15:50 -0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,12 @@ router.get('/cool', function(req, res, next) {
   res.send('You are so cool');
 });
 
+router.get('/cool/beach', function(req, res, next) {
+  res.send('Adoro praia');
+});
+
+router.get('/ifc', function(req, res, next) {
+  res.send('Bem vindo ao IFC');
+});
+
 module.exports = router;
